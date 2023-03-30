666ff8832b198457fbc44a9f92eaf0d21d2739ce
Niharf14 <niharf14@gmail.com>
2023-03-30 18:27:32 +0530

    added products summary

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -4,11 +4,21 @@ import './Shop.css'
 
 const shop = () => {
     const [products, setProducts] = useState([]);
+    const [cart, setCart] = useState([]);
     useEffect( () => {
         fetch('products.json')
             .then(res => res.json())
             .then(data => setProducts(data))
     }, []);
+    
+    const handleAddToCart = (product) => {
+        // console.log('ok');
+        // console.log(product);
+        const newCart = [...cart, product];
+        setCart(newCart);
+
+    };
+
     return (
         <div className='shop-container'>
             <div className="products-container">
@@ -16,11 +26,15 @@ const shop = () => {
                     products.map(product => <Product
                     key={product.id}
                     product = {product}
+                    handleAddToCart={handleAddToCart}
                     ></Product>)
                 }
             </div>
             <div className="cart-container">
-                <h2>order Summary</h2>
+                <h4>Order Summary</h4>
+                <div className='cart-info'>
+                    <p>Selected Items: {cart.length}</p>
+                </div>
             </div>
         </div>
     );
